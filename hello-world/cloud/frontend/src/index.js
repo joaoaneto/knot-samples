c896a6eb2e221107e79ecde4eef47bc638f6e943
João Neto <joao.alexandre@cesar.org.br>
2019-07-08 10:08:17 -0300

    hello-world: ui: Update service worker

--- hello-world/cloud/frontend/src/index.js
+++ hello-world/cloud/frontend/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+serviceWorker.unregister()
